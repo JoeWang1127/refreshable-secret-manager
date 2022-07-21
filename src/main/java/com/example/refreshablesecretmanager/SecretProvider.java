37d0976381c1c53c26bb59a9613d1a854a6c202b
Joe Wang <joewa@google.com>
2022-07-21 15:30:22 -0400

    remove refresh scope

--- src/main/java/com/example/refreshablesecretmanager/SecretProvider.java
+++ src/main/java/com/example/refreshablesecretmanager/SecretProvider.java
@@ -2,11 +2,9 @@ package com.example.refreshablesecretmanager;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @ConstructorBinding
 @ConfigurationProperties(prefix = "application")
-@RefreshScope
 public class SecretProvider {
 
   private String secret;
